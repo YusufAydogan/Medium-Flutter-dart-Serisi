ba99ec2b4a3d75cc00f60f7e20f8c6ec96328aed
Yusuf Aydoğan <79870923+MrCodeEngineer@users.noreply.github.com>
2022-08-12 08:42:05 +0300

    Create if-else-Yapısı.dart

---
+++ if-else-Yapısı.dart
@@ -0,0 +1,22 @@
+// Burada sadece if-else kullandık
+if(şart){
+//Yapılması istenilen işlemler
+}else{
+//Yapılması istenilen işlemler}
+
+
+  
+// Burada da else if yapısını kullandık.
+
+if(şart){
+//Yapılması istenilen işlemler
+}
+else if(if şartı sağlanmıyorsa bakılması istenilen şart){
+//Yapılması istenile işlemler
+}
+else if(eğer diğer şartlar sağlanmıyorsa bakılması istenilen şart){
+//Yapılması istenilen işlemler
+}
+else{
+//Yapılması istenile işlemler
+}
